dd0c5262ba669b6aa3668f52762849484afdbfc5
Robert Condie <rcondie@affirma.com>
2019-12-23 10:24:17 -0800

    First Commit

---
+++ Assets/Monster.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public int attack;
+    public int defense;
+    public int hp;
+    public int speed;
+    public int attackGrowth;
+    public int defenseGrowth;
+    public int hpGrowth;
+    public int speedGrowth;
+    public Sprite pic;
+    public int type;
+    public string nickname;
+    public Monster(int attackGrow, int defenseGrow, int hpGrow, int speedGrow, string nick, Sprite picture, int typeOf){
+        SetGrowth(attackGrow, defenseGrow, hpGrow, speedGrow);
+        nickname = nick;
+        pic = picture;
+        type = typeOf;
+    }
+    public int RandomNumber(){  
+        Random random = new Random();  
+        return Random.Range(0, 100);
+    }
+    public void SetGrowth(int attackGrow, int defenseGrow, int hpGrow, int speedGrow){
+        int rand = RandomNumber();
+        if(rand < 70){
+            attackGrowth = attackGrow;
+        }
+        else if(rand < 85){
+            attackGrowth = attackGrow - 1;
+        }
+        else{
+            attackGrowth = attackGrow + 1;
+        }
+
+        rand = RandomNumber();
+        if(rand < 70){
+            defenseGrowth = defenseGrow;
+        }
+        else if(rand < 85){
+            defenseGrowth = defenseGrow - 1;
+        }
+        else{
+            defenseGrowth = defenseGrow + 1;
+        }
+
+        rand = RandomNumber();
+        if(rand < 70){
+            hpGrowth = hpGrow;
+        }
+        else if(rand < 85){
+            hpGrowth = hpGrow - 1;
+        }
+        else{
+            hpGrowth = hpGrow + 1;
+        }
+
+        rand = RandomNumber();
+        if(rand < 70){
+            speedGrowth = speedGrow;
+        }
+        else if(rand < 85){
+            speedGrowth = speedGrow - 1;
+        }
+        else{
+            speedGrowth = speedGrow + 1;
+        }
+    }
+}
---
+++ Assets/TurnBased.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnBased : MonoBehaviour
+{
+    enum Type
+    {
+        Circle = 0,
+        Square = 1,
+        Triangle = 2,
+        Line = 3,
+        Hexagon = 4,
+        Octagon = 5
+    }
+    public Sprite squarePic;
+    public Sprite trianglePic;
+    public Sprite circlePic;
+    public Sprite linePic;
+    public Sprite hexagonPic;
+    public Sprite octagonPic;
+    public Monster CreateSquare(){
+        return new Monster(2, 3, 3, 1, "Square", squarePic, (int)Type.Square);
+    }
+    public Monster CreateCircle(){
+        return new Monster(3, 1, 1, 3, "Circle", circlePic, (int)Type.Circle);
+    }
+    public Monster CreateTriangle(){
+        return new Monster(2, 2, 2, 2, "Triangle", trianglePic, (int)Type.Triangle);
+    }
+    public Monster CreateLine(){
+        return new Monster(1, 5, 3, 1, "Line", linePic, (int)Type.Line);
+    }
+    public Monster CreateHexagon(){
+        return new Monster(2, 2, 2, 5, "Hexagon", hexagonPic, (int)Type.Hexagon);
+    }
+    public Monster CreateOctagon(){
+        return new Monster(5, 2, 2, 1, "Octagon", octagonPic, (int)Type.Octagon);
+    }
+}
