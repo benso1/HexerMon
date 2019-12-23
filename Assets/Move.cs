4605daba9b7b61d68ac63774d05af93a18134851
Robert Condie <rcondie@affirma.com>
2019-12-23 11:33:17 -0800

    Created Basic Move Setup

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -4,7 +4,40 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    Move(){
-        
+    public enum MoveType{
+        Punch = 0,
+        SelfDestruct = 1,
+		ShieldBash = 2,
+		Net = 3,
+		Splash = 4,
+		Stall = 5,
+    }
+    public int dmg;
+    public int selfDmg;
+    public int type;
+    Move(int typeOf){
+        dmg = 0;
+        selfDmg = 0;
+        type = typeOf;
+    }
+    public void UpdateMove(){
+        switch(type){
+            case ((int)MoveType.Punch):
+                dmg = 1;
+                selfDmg = 0;
+                break;
+            case ((int)MoveType.SelfDestruct):
+                break;
+            case ((int)MoveType.ShieldBash):
+                break;
+            case ((int)MoveType.Net):
+                break;
+            case ((int)MoveType.Splash):
+                dmg = 0;
+                selfDmg = 0;
+                break;
+            case ((int)MoveType.Stall):
+                break;
+        }
     }
 }
--- Assets/World.cs
+++ Assets/World.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class World : MonoBehaviour
 {
-    enum Type{
+    enum Shape{
         Circle = 0,
         Square = 1,
         Triangle = 2,
@@ -19,21 +19,21 @@ public class World : MonoBehaviour
     public Sprite hexagonPic;
     public Sprite octagonPic;
     public Monster CreateSquare(int level){
-        return new Monster(2, 3, 3, 1, "Square", squarePic, (int)Type.Square, level);
+        return new Monster(2, 3, 3, 1, "Square", squarePic, (int)Shape.Square, level);
     }
     public Monster CreateCircle(int level){
-        return new Monster(3, 1, 1, 3, "Circle", circlePic, (int)Type.Circle, level);
+        return new Monster(3, 1, 1, 3, "Circle", circlePic, (int)Shape.Circle, level);
     }
     public Monster CreateTriangle(int level){
-        return new Monster(2, 2, 2, 2, "Triangle", trianglePic, (int)Type.Triangle, level);
+        return new Monster(2, 2, 2, 2, "Triangle", trianglePic, (int)Shape.Triangle, level);
     }
     public Monster CreateLine(int level){
-        return new Monster(1, 5, 3, 1, "Line", linePic, (int)Type.Line, level);
+        return new Monster(1, 5, 3, 1, "Line", linePic, (int)Shape.Line, level);
     }
     public Monster CreateHexagon(int level){
-        return new Monster(2, 2, 2, 5, "Hexagon", hexagonPic, (int)Type.Hexagon, level);
+        return new Monster(2, 2, 2, 5, "Hexagon", hexagonPic, (int)Shape.Hexagon, level);
     }
     public Monster CreateOctagon(int level){
-        return new Monster(5, 2, 2, 1, "Octagon", octagonPic, (int)Type.Octagon, level);
+        return new Monster(5, 2, 2, 1, "Octagon", octagonPic, (int)Shape.Octagon, level);
     }
 }
